96d958efc4e08a09edbe7672c84ead7c3d3371c2
Abhijith Chatra <abchatra@microsoft.com>
2020-12-03 15:29:24 -0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -6,25 +6,25 @@
  */
 //% color="#AA278D" weight=100
 namespace Area {
-    //% block "circle radius %radius"
+    //% block="circle radius $radius"
     //% radius.defl=100
     //% radius.min=0 radius.max=1000
     export function circle(radius:number) {
         return radius*radius*Math.PI;
     }
 
-    //% block "triangle length %length width %width"
+    //% block="rectangle length $length width $width"
     export function rectangle(length: number, width: number) {
         return length * width;
     }
 
-    //% block "square length %length"
+    //% block="square length $length"
     export function square(length: number) {
         return length * length;
     }
 
 
-    //% block "triangle base %base height %height"
+    //% block="triangle base $base height $height"
     export function triangle(base:number, height: number) {
         return 0.5 * base * height;
     }
